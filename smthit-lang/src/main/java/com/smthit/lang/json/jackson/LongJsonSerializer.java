b604541b6b6de73e776efa6c29ade66451082ec7
bitlong <bitlong@126.com>
2018-04-26 12:26:43 +0800

    jackson序列化long型，丢失精度

---
+++ smthit-lang/src/main/java/com/smthit/lang/json/jackson/LongJsonSerializer.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package com.smthit.lang.json.jackson;
+
+import java.io.IOException;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+
+/**
+ * @author Bean
+ *
+ */
+public class LongJsonSerializer extends JsonSerializer<Long> {
+    @Override
+    public void serialize(Long value, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException, JsonProcessingException {
+        String text = (value == null ? null : String.valueOf(value));
+        if (text != null) {
+            jsonGenerator.writeString(text);
+        }
+    }
+}
