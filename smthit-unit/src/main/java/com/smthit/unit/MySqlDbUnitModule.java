f934d4a6b3dcef267f7540264ae3bb14929cd5f0
bitlong <bitlong@126.com>
2018-08-24 20:55:03 +0800

    .

---
+++ smthit-unit/src/main/java/com/smthit/unit/MySqlDbUnitModule.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package com.smthit.unit;
+
+import org.dbunit.database.DatabaseConfig;
+import org.dbunit.ext.mysql.MySqlDataTypeFactory;
+import org.dbunit.ext.mysql.MySqlMetadataHandler;
+import org.unitils.dbunit.DbUnitModule;
+import org.unitils.dbunit.util.DbUnitDatabaseConnection;
+
+/**
+ * @author Bean
+ *
+ */
+public class MySqlDbUnitModule extends DbUnitModule {
+	@Override
+	public DbUnitDatabaseConnection getDbUnitDatabaseConnection(final String schemaName) {
+		DbUnitDatabaseConnection result = dbUnitDatabaseConnections.get(schemaName);
+		
+		if (null != result) {
+			return result;
+		}
+		
+		result = super.getDbUnitDatabaseConnection(schemaName);
+		
+		result.getConfig().setProperty(DatabaseConfig.PROPERTY_DATATYPE_FACTORY, new MySqlDataTypeFactory());
+		result.getConfig().setProperty(DatabaseConfig.PROPERTY_METADATA_HANDLER, new MySqlMetadataHandler());
+		
+		return result;
+	}
+}
